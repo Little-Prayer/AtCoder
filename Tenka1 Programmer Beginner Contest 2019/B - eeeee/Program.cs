0efa8bcf1d39e1c938ecbd4db2ae758584da3650
杏子 佐倉 <parquets.poekago@gmail.com>
2019-04-21 19:41:41 +0900

    Tenka1 Programmer BC AとBだけAC

---
+++ Tenka1 Programmer Beginner Contest 2019/B - eeeee/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace B___eeeee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var N = int.Parse(Console.ReadLine());
+            var S = Console.ReadLine();
+            var K = int.Parse(Console.ReadLine());
+
+            char check = S[K-1];
+            string result = "";
+
+            for (int i = 0 ; i < N ; i++)
+            {
+                if(S[i] == check)
+                {
+                    result += check;
+                }else{
+                    result += "*";
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Tenka1 Programmer Beginner Contest 2019/C - Stones/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace C___Stones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var N = int.Parse(Console.ReadLine());
+            var S = Console.ReadLine();
+
+            var stoneGroup = new int[100000];
+            var stoneCount = 1;
+            var groupCount = 0;
+
+            for (int i = 1 ; i < N ; i++)
+            {
+                if(S[i]==S[i-1])
+                {
+                    stoneCount++;
+                }else{
+                    stoneGroup[groupCount] = stoneCount;
+                    stoneCount = 1;
+                }
+            }
+
+            int j;
+            int result = 0;
+            if(S[0] == '#')
+            {
+                j = 0;
+            }else{
+                j = 1;
+            }
+
+            for(;j<groupCount;j+=2)
+            {
+                if(stoneGroup[j]<=stoneGroup[j+1])
+                {
+                    result += stoneGroup[j];
+                }else{
+                    result += stoneGroup[j+1];
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
