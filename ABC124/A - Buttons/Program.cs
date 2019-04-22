a2434e12f4e29cad17206ac861f4239ad5d9943d
Koki Sato <koki.sato@toyoshima.co.jp>
2019-04-22 17:06:49 +0900

    ABC124-A AC

---
+++ ABC124/A - Buttons/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace A___Buttons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var read = Console.ReadLine().Split(' ');
+            var A = int.Parse(read[0]);
+            var B = int.Parse(read[1]);
+
+            if(A==B)
+            {
+                Console.WriteLine(A*2);
+            }else{
+                var large = Math.Max(A,B);
+                Console.WriteLine(large+(large-1));
+            }
+                
+        }
+    }
+}
---
+++ ABC124/B - Great Ocean View/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace B___Great_Ocean_View
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
