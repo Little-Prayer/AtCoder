99259ed8c0b21b388328292887b35eac68091836
Koki Sato <koki.sato@toyoshima.co.jp>
2019-05-09 17:25:11 +0900

    ABC054-C仕掛

---
+++ ABC054/C - One-stroke Path/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace C___One_stroke_Path
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var read = Array.ConvertAll(Console.ReadLine().Split(' '),int.Parse);
+            var N = read[0];
+            var M = read[1];
+
+            var nodes = new List<int>[N+1];
+            for(int i = 1 ; i < N+1 ; i++) nodes[i] = new List<int>();
+
+            for(int j = 0 ; j < M ; j++)
+            {
+                read = Array.ConvertAll(Console.ReadLine().Split(' '),int.Parse);
+                nodes[read[0]].Add[read[1]];
+                nodes[read[1]].Add[read[0]];
+            }
+        }
+    }
+}
