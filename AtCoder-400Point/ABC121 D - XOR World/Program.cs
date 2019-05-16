7fdb5133a3dc93189ef5f53b66c6e4fb10b74cea
Koki Sato <koki.sato@toyoshima.co.jp>
2019-05-16 14:23:40 +0900

    ABC121-D 自力AC

--- AtCoder-400Point/ABC121 D - XOR World/Program.cs
+++ AtCoder-400Point/ABC121 D - XOR World/Program.cs
@@ -6,7 +6,23 @@ namespace ABC121_D___XOR_World
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var AB = Array.ConvertAll(Console.ReadLine().Split(' '),long.Parse);
+            
+            var digit = 1;
+            long result = 0;
+            while(digit < 1000)
+            {
+                result += ((countBinary1(AB[1],digit)-countBinary1(AB[0]-1,digit)) % 2) * (long)Math.Pow(2,digit-1);
+                digit++;
+            }
+            Console.WriteLine(result);
+        }
+
+        static long countBinary1(long number,int digit)
+        {
+            long a = (long)(number/Math.Pow(2,digit)) * (long)Math.Pow(2,digit-1);
+            long b = Math.Max(number % (long)Math.Pow(2,digit) - (long)Math.Pow(2,digit-1) + 1,0);
+            return a+b;
         }
     }
 }
