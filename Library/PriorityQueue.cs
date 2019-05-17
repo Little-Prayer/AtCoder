3905ace64628259ba9cb3004fff423c4668870f3
Koki Sato <koki.sato@toyoshima.co.jp>
2019-05-17 17:30:22 +0900

    優先度付きキュー仕掛

--- Library/PriorityQueue.cs
+++ Library/PriorityQueue.cs
@@ -18,11 +18,32 @@ class PriorityQueue<T> where T : IComparable
     }
     public T Pop()
     {
-        
+        T returnNode = list(0);
+        list(0) = list(list.Count-1);
+        list.RemoveAt(list.Count-1);
+        int root = 0;
+        while(root*2 + 1 < list.Count)
+        {
+            int child = root * 2 + 1;
+            if(compare(list(child+1),list(child)) > 0) child += 1;
+            
+        }
     }
     public void Push(T t)
     {
-
+        this.list.Add(t);
+        var addPosition = list.Count - 1;
+        while(addPosition > 0)
+        {
+            var parent = (int)(addPosition-1)/2;
+            if(compare(list(addPosition),list(parent)) > 0 )
+            {
+                swap(addPosition,parent);
+                addPosition = parent;
+            }else{
+                break;
+            }
+     }
     }
     private int compare(T x,T y)
     {
