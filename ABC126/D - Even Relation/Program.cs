0abe831e4de82bfffde4a8a0b91badd92850049c
Koki Sato <koki.sato@toyoshima.co.jp>
2019-05-28 16:30:17 +0900

    ABC126D 自力AC

--- ABC126/D - Even Relation/Program.cs
+++ ABC126/D - Even Relation/Program.cs
@@ -9,15 +9,15 @@ namespace D___Even_Relation
         static void Main(string[] args)
         {
             var N = int.Parse(Console.ReadLine());
-            var nodes = new node[N];
+            var nodes = new node[N+1];
             for(int i = 1 ; i < N+1 ; i++ ) nodes[i] = new node();
-            for(int i = 0 ; i < N ; i++)
+            for(int i = 0 ; i < N-1 ; i++)
             {
                 var read = Array.ConvertAll(Console.ReadLine().Split(' '),int.Parse);
                 nodes[read[0]].children.Add(read[1]);
                 nodes[read[1]].children.Add(read[0]);
-                nodes[read[0]].costs.Add(read[2]%2);
-                nodes[read[1]].costs.Add(read[2]%2);
+                nodes[read[0]].isCostOdd.Add(read[2] % 2 == 1);
+                nodes[read[1]].isCostOdd.Add(read[2] % 2 == 1);
             }
 
             var queue = new Queue<node>();
@@ -32,9 +32,15 @@ namespace D___Even_Relation
                     var searchNode = nodes[currentNode.children[i]];
                     if(!searchNode.visited)
                     {
-                        
+                        searchNode.isOdd = currentNode.isCostOdd[i] ^ currentNode.isOdd;
+                        queue.Enqueue(searchNode);
                     }
                 }
+            }
+            foreach(node n in nodes)
+            {
+                if(n == null) continue;
+                Console.WriteLine(n.isOdd ? "0" : "1");
             }
         }
     }
@@ -44,11 +50,11 @@ namespace D___Even_Relation
         public bool isOdd{get;set;}
 
         public List<int> children{get;set;}
-        public List<int> costs{get; set;}
+        public List<bool> isCostOdd{get; set;}
         
         public node(){
             children = new List<int>();
-            costs = new List<int>();
+            isCostOdd = new List<bool>();
         }
         
     }
