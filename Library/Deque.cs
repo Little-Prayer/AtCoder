418b3d91af901d821833fce2471ac28dedb0eb25
Koki Sato <koki.sato@toyoshima.co.jp>
2019-05-30 17:28:23 +0900

    Deque仕掛

--- Library/Deque.cs
+++ Library/Deque.cs
@@ -5,11 +5,28 @@ class Deque<T>
     T[] buffer;
     int capacity, count, front;
 
-    public Deque(int _capacity)
+    public Deque(int _capacity){buffer = new T[capacity = _capacity];}
+    public Deque(){buffer = new T[capacity = 16];}
+
+    public void PushFront(T item)
     {
-        capacity = _capacity;
-        count = 0;
-        front = 0;
-        buffer = new T[capacity];
+
+    }
+    public void PushBack(T item)
+    {
+
+    }
+    public T PopFront()
+    {
+
+    }
+    public T PopBack()
+    {
+
+    }
+
+    private void extend()
+    {
+        
     }
 }
