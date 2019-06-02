0a1dbf22eec1c4f4d4dcd9f04d5255ebd5e65c6f
杏子 佐倉 <parquets.poekago@gmail.com>
2019-06-02 23:01:18 +0900

    AGC034-A AC

---
+++ AGC034/A - Kenken Race/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace A___Kenken_Race
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var read = Array.ConvertAll(Console.ReadLine().Split(' '),int.Parse);
+            var N = read[0];
+            var A = read[1];
+            var B = read[2];
+            var C = read[3];
+            var D = read[4];
+            var S = Console.ReadLine();
+
+            var subS = S.Substring(A-1,Math.Max(C,D)-A+1);
+            Console.WriteLine(solver(A-1,B-A,C-A,D-A,subS) ? "Yes":"No");            
+        }
+        static bool solver(int A,int B,int C,int D,string S)
+        {
+            if(S.IndexOf("##") >= 0) return false;
+            if(C<D) return true;
+            if(S.Substring(B,D-B+1).IndexOf("...")>=0) return true;
+            if(S.Substring(D-1,3) == "...") return true;
+            if(S.Substring(B-1,3) == "...") return true;
+            return false;
+        }
+    }
+}
---
+++ AGC034/B - ABC/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace B___ABC
+{
+    class Program
+    {
+        static int count;
+        static Regex rx;
+        static void Main(string[] args)
+        {
+            var S = Console.ReadLine();
+            rx = new Regex("ABC");
+            count = 0;
+            var subS = S.Split(new string[]{"AC","BA","BB","CC"},StringSplitOptions.RemoveEmptyEntries);
+
+            foreach(string str in subS)
+            {
+                replaceABC(str);
+            }
+            Console.WriteLine(count);
+        }
+        static string replaceABC(string S)
+        {
+            int _count = rx.Matches(S).Count;
+            if(_count > 0)
+            {
+                count += _count;
+                return replaceABC(S.Replace("ABC","BCA"));
+            }else{
+                return "0";
+            }
+        }
+    }
+}
