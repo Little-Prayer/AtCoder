4f455f21590a18d450909d89c37cbbbca1ec2b93
杏子 佐倉 <parquets.poekago@gmail.com>
2019-06-17 21:23:39 +0900

    ABC130参加

---
+++ ABC130/A - Rounding/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace A___Rounding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var XA = Array.ConvertAll(Console.ReadLine().Split(' '),int.Parse);
+            Console.WriteLine(XA[0] < XA[1] ? 0:10);
+        }
+    }
+}
---
+++ ABC130/D - Enough Array/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace D___Enough_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var NK = Array.ConvertAll(Console.ReadLine().Split(' '),long.Parse);
+            var N = NK[0];
+            var K = NK[1];
+
+            var Ai = Array.ConvertAll(Console.ReadLine().Split(' '),long.Parse);
+            var acum = new long[N+1];
+            acum[0] = 0;
+            for(int i = 1 ; i < N+1 ; i++) acum[i] = acum[i-1] + Ai[i-1];
+
+            var result = new long[N+1];
+            var lead = 0;
+            var follow = 0;
+            for(;lead < N+1 ; lead++)
+            {
+                if(acum[lead] - acum[follow] < K)continue;
+                for(;follow < lead && acum[lead] - acum[follow]>=K;follow++) result[follow] = N + 1 - lead;
+            }
+            Console.WriteLine(result.Sum());
+        }
+    }
+}
