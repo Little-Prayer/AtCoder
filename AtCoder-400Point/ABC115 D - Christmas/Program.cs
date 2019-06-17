db4ba9806209bde8418ce47affe9f789f0bf2fcc
Koki Sato <koki.sato@toyoshima.co.jp>
2019-06-17 13:17:30 +0900

    ABC115 D AC

---
+++ AtCoder-400Point/ABC115 D - Christmas/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ABC115_D___Christmas
+{
+    class Program
+    {
+        static long[] burger;
+        static long[] patty;
+        static void Main(string[] args)
+        {
+            var NX = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse);
+            var N = NX[0];
+            var X = NX[1];
+
+            burger = new long[N + 1];
+            burger[0] = 1;
+            for (int i = 1; i < N + 1; i++) burger[i] = burger[i - 1] * 2 + 3;
+
+            patty = new long[N + 1];
+            patty[0] = 1;
+            for (int i = 1; i < N + 1; i++) patty[i] = patty[i - 1] * 2 + 1;
+
+            Console.WriteLine(Eat(N, X));
+        }
+
+        static long Eat(long N, long X)
+        {
+            if (N == 0) return X == 1 ? 1 : 0;
+            if (X <= burger[N - 1] + 1) return Eat(N - 1, X - 1);
+            if (X == burger[N]) return patty[N];
+            return 1 + patty[N - 1] + Eat(N - 1, X - 2 - burger[N - 1]);
+        }
+    }
+}
