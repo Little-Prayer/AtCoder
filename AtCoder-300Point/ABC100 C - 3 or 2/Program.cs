f5585b7eb1db0468fd1376390a5506064650380c
Koki Sato <koki.sato@toyoshima.co.jp>
2019-06-20 15:56:44 +0900

    ABC100 C AC

---
+++ AtCoder-300Point/ABC100 C - 3 or 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ABC100_C___3_or_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+            var ai = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+            var count = 0;
+            for (int i = 0; i < ai.Length; i++)
+            {
+                var a = ai[i];
+                while (a % 2 == 0)
+                {
+                    a /= 2;
+                    count += 1;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
