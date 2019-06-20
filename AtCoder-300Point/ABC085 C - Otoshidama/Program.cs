256a0c1f911bf1d70eb761c9d9a9135e59298178
Koki Sato <koki.sato@toyoshima.co.jp>
2019-06-20 12:23:46 +0900

    ABC085 C AC

---
+++ AtCoder-300Point/ABC085 C - Otoshidama/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ABC085_C___Otoshidama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(solve());
+        }
+        static string solve()
+        {
+            var NY = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            var N = NY[0];
+            var Y = NY[1] / 1000;
+
+            for (int k10 = 0; k10 <= N; k10++)
+                for (int k5 = 0; k5 <= N - k10; k5++)
+                    if (k10 * 10 + k5 * 5 + (N - k10 - k5) == Y)
+                        return $"{k10} {k5} {N - k10 - k5}";
+
+            return "-1 -1 -1";
+        }
+    }
+}
