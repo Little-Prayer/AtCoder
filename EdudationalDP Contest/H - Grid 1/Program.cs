792008b6521ae7e0e455d5fe04f75808c08ebabc
Koki Sato <koki.sato@toyoshima.co.jp>
2019-06-26 10:04:13 +0900

    EDPC-H AC

--- EdudationalDP Contest/H - Grid 1/Program.cs
+++ EdudationalDP Contest/H - Grid 1/Program.cs
@@ -10,15 +10,15 @@ namespace H___Grid_1
             var H = HW[0];
             var W = HW[1];
 
-            int MOD = 1000000007;
+            long MOD = 1000000007;
 
             var grid = new string[H];
             for (int i = 0; i < H; i++) grid[i] = Console.ReadLine();
 
-            var DP = new int[H, W];
+            var DP = new long[H, W];
             for (int i = 0; i < H; i++) for (int j = 0; j < W; j++) DP[i, j] = 0;
-            for (int i = 0; i < H; i++) if (grid[i][1] == '.') DP[i, 1] = 1; else break;
-            for (int j = 0; j < W; j++) if (grid[1][j] == '.') DP[1, j] = 1; else break;
+            for (int i = 0; i < H; i++) if (grid[i][0] == '.') DP[i, 0] = 1; else break;
+            for (int j = 0; j < W; j++) if (grid[0][j] == '.') DP[0, j] = 1; else break;
 
             for (int i = 1; i < H; i++)
                 for (int j = 1; j < W; j++)
