c7ec270d8e87a110eee906b7e8cce1338c5b2ebf
Koki Sato <koki.sato@toyoshima.co.jp>
2019-06-26 11:03:31 +0900

    EDPC - I AC

---
+++ EdudationalDP Contest/I - Coins/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace I___Coins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var N = int.Parse(Console.ReadLine());
+            var probs = Array.ConvertAll(Console.ReadLine().Split(' '), decimal.Parse);
+            var DP = new decimal[N + 1, 2 * N + 1];
+            DP[1, N + 1] = probs[0];
+            DP[1, N - 1] = 1 - probs[0];
+
+            for (int i = 1; i < N; i++)
+            {
+                for (int j = N - i; j <= N + i; j++)
+                {
+                    DP[i + 1, j + 1] += DP[i, j] * probs[i];
+                    DP[i + 1, j - 1] += DP[i, j] * (1 - probs[i]);
+                }
+            }
+            decimal result = 0;
+            for (int i = N; i <= 2 * N; i++) result += DP[N, i];
+
+            Console.WriteLine(result);
+        }
+    }
+}
