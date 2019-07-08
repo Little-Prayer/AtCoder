028c0ff11e0c164ff53265d356c5d1be7409a1f8
Koki Sato <koki.sato@toyoshima.co.jp>
2019-07-08 19:15:54 +0900

    EDPC K 解説見てAC

---
+++ EdudationalDP Contest/K - Stones/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace K___Stones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var NK = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            var N = NK[0];
+            var K = NK[1];
+            var picks = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+            var DP = new bool[K + 1];
+            for (int i = 0; i < K + 1; i++) DP[i] = false;
+
+            for (int i = K; i >= 0; i--)
+            {
+                for (int j = 0; j < N && picks[j] <= K - i; j++)
+                {
+                    if (!DP[i + picks[j]])
+                    {
+                        DP[i] = true;
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine(DP[0] ? "First" : "Second");
+        }
+    }
+}
