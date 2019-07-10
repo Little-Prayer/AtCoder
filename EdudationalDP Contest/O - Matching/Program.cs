dd31a215c300545e92a8dbd7a677df65d45c4101
Koki Sato <koki.sato@toyoshima.co.jp>
2019-07-10 16:55:18 +0900

    EDPC - O　仕掛り

---
+++ EdudationalDP Contest/O - Matching/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace O___Matching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var N = int.Parse(Console.ReadLine());
+            var matching = new List<List<bool>>();
+            for (int i = 0; i < N; i++) matching[i] = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse).Select(s => s == 1).ToList();
+
+        }
+    }
+}
