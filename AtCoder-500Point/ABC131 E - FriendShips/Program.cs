1f47a8574c52d49a91223e8dcb7c300802d2b9cd
Koki Sato <koki.sato@toyoshima.co.jp>
2019-07-11 17:41:06 +0900

    ABC131 E 仕掛り

--- AtCoder-500Point/ABC131 E - FriendShips/Program.cs
+++ AtCoder-500Point/ABC131 E - FriendShips/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace ABC131_E___FriendShips
 {
@@ -6,7 +7,30 @@ namespace ABC131_E___FriendShips
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine(solver());
+        }
+        static string solver()
+        {
+            var NK = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            var N = NK[0];
+            var K = NK[1];
+
+            if (K > (N - 1) * (N - 2) / 2) return "-1";
+
+            var graph = new StringBuilder();
+            for (int i = 2; i <= N; i++) graph.AppendLine($"1 {i}");
+
+            var counter = 0;
+            for (int i = 2; i < N; i++)
+            {
+                for (int j = i + 1; j <= N; j++)
+                {
+                    if (K == (N - 1) * (N - 2) / 2 - counter) return graph.ToString().TrimEnd('\r', '\n');
+                    graph.AppendLine($"{i} {j}");
+                    counter += 1;
+                }
+            }
+            return "";
         }
     }
 }
