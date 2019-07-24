c51de3d621843bc8ac9d309c1a2458814c81993a
Koki Sato <agricola.basic@gmail.com>
2019-07-24 10:07:30 +0900

    ABC104D別解　AC

--- AtCoder-400Point/ABC104 D - We Love ABC別解/Program.cs
+++ AtCoder-400Point/ABC104 D - We Love ABC別解/Program.cs
@@ -9,9 +9,9 @@ namespace ABC104_D___We_Love_ABC別解
         static void Main(string[] args)
         {
             var S = Console.ReadLine();
-            var accumA = new int[S.Length + 1];
-            var accumC = new int[S.Length + 1];
-            var accumQ = new int[S.Length + 1];
+            var accumA = new long[S.Length + 1];
+            var accumC = new long[S.Length + 1];
+            var accumQ = new long[S.Length + 1];
             for (int i = 0; i < S.Length; i++)
             {
                 accumA[i + 1] = accumA[i] + (S[i] == 'A' ? 1 : 0);
@@ -27,19 +27,26 @@ namespace ABC104_D___We_Love_ABC別解
             ModInt power3Qminus2 = power3Q / 9;
             ModInt power3Qminus3 = power3Q / 27;
 
+
             ModInt result = 0;
             for (int i = 1; i < S.Length; i++)
             {
                 if (S[i - 1] == 'B')
                 {
-
+                    result += accumA[i - 1] * (totalC - accumC[i]) * power3Q;
+                    result += accumA[i - 1] * (totalQ - accumQ[i]) * power3Qminus1;
+                    result += accumQ[i - 1] * (totalC - accumC[i]) * power3Qminus1;
+                    result += accumQ[i - 1] * (totalQ - accumQ[i]) * power3Qminus2;
                 }
                 if (S[i - 1] == '?')
                 {
-
+                    result += accumA[i - 1] * (totalC - accumC[i]) * power3Qminus1;
+                    result += accumA[i - 1] * (totalQ - accumQ[i]) * power3Qminus2;
+                    result += accumQ[i - 1] * (totalC - accumC[i]) * power3Qminus2;
+                    result += accumQ[i - 1] * (totalQ - accumQ[i]) * power3Qminus3;
                 }
             }
-
+            Console.WriteLine(result.ToString());
         }
     }
     //パクリ元　https://atcoder.jp/contests/arc067/submissions/5337727
