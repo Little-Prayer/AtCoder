c8abc5260db010d3f60cb1f590ca0dda6f08bee4
Koki Sato <agricola.basic@gmail.com>
2019-07-26 16:57:06 +0900

    ARC067D 自力AC

---
+++ AtCoder-500Point/ARC067 D - Walk and Teleport/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ARC067_D___Walk_and_Teleport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var NAB = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse);
+            var N = NAB[0];
+            var A = NAB[1];
+            var B = NAB[2];
+            var Xi = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse);
+            long result = 0;
+            for (int i = 1; i < N; i++) result += Math.Min(B, (Xi[i] - Xi[i - 1]) * A);
+            Console.WriteLine(result);
+        }
+    }
+}
