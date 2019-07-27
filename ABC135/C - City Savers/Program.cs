25035a5935b2592fe4efdd13be22fb76476e1715
39raKyouko <agricola.basic@gmail.com>
2019-07-27 23:34:29 +0900

    ABC135参加、Dは未AC

---
+++ ABC135/C - City Savers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace C___City_Savers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var N = long.Parse(Console.ReadLine());
+            var Ai = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse);
+            var Bi = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse);
+            long result = 0;
+            for (int i = 0; i < N; i++)
+            {
+                if (Ai[i] >= Bi[i])
+                {
+                    result += Bi[i];
+                }
+                else
+                {
+                    result += Ai[i];
+                    long overkill = Bi[i] - Ai[i];
+                    if (Ai[i + 1] >= overkill)
+                    {
+                        result += overkill;
+                        Ai[i + 1] -= overkill;
+                    }
+                    else
+                    {
+                        result += Ai[i + 1];
+                        Ai[i + 1] = 0;
+                    }
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
