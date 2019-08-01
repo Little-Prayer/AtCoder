3cfccd5270d188aa69256807aafea2a4914ac8dc
Koki Sato <agricola.basic@gmail.com>
2019-08-01 17:26:04 +0900

    ARC071 D 仕掛り

---
+++ AtCoder-500Point/ARC071 D - 井井井  ###/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ARC071_D___井井井_____
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long MOD = 1000000007;
+            var nm = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse);
+            var Xi = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse);
+            var Yi = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse);
+
+        }
+    }
+}
