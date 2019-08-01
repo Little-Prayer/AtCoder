4df028c100ad4d5c19cd8f350801ac59ebe50dbf
Koki Sato <agricola.basic@gmail.com>
2019-08-01 11:45:33 +0900

    ARC064 C 自力AC

---
+++ AtCoder-300Point/ARC064 C - Boxes and Candies/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ARC064_C___Boxes_and_Candies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var NX = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse);
+            var N = NX[0];
+            var X = NX[1];
+            var Ai = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse);
+            long result = 0;
+            for (long i = 1; i < N; i++)
+            {
+                if (Ai[i] + Ai[i - 1] <= X) continue;
+                long difference = Ai[i] + Ai[i - 1] - X;
+                result += difference;
+                Ai[i] = Math.Max(Ai[i] - difference, 0);
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
