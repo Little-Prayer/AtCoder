6abd43f04c66b1bf07d049ef006a72adeec84a0f
Koki Sato <agricola.basic@gmail.com>
2019-08-01 11:01:43 +0900

    ARC064 D 解説見てAC

---
+++ AtCoder-500Point/ARC064 D - An Ordinary Game/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ARC064_D___An_Ordinary_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var S = Console.ReadLine();
+            Console.WriteLine(((S.Length % 2) == 0) ^ (S[0] == S[S.Length - 1]) ? "Second" : "First");
+        }
+    }
+}
