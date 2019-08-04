2e7ea813f81e83779de2c7eeecc45beaf04d1848
39raKyouko <agricola.basic@gmail.com>
2019-08-04 22:27:15 +0900

    ABC136参加　A-D完

---
+++ ABC136/C - Build Stairs/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace C___Build_Stairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(solver() ? "Yes" : "No");
+        }
+        static bool solver()
+        {
+            var N = int.Parse(Console.ReadLine());
+            var Hi = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse);
+            for (int i = N - 1; i > 0; i--) if (Hi[i - 1] > Hi[i]) Hi[i - 1]--;
+            for (int i = 0; i < N - 1; i++) if (Hi[i + 1] < Hi[i]) return false;
+            return true;
+        }
+    }
+}
---
+++ ABC136/D - Gathering Children/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace D___Gathering_Children
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var S = Console.ReadLine();
+            var result = new int[S.Length];
+            var countR = 0;
+            for (int i = 0; i < S.Length; i++)
+            {
+                if (S[i] == 'R') countR++;
+                if (S[i] == 'L')
+                {
+                    result[i] += countR / 2;
+                    result[i - 1] += countR / 2;
+                    if ((countR % 2) == 1) result[i - 1] += 1;
+                    countR = 0;
+                }
+            }
+            var countL = 0;
+            for (int i = S.Length - 1; i >= 0; i--)
+            {
+                if (S[i] == 'L') countL++;
+                if (S[i] == 'R')
+                {
+                    result[i] += countL / 2;
+                    result[i + 1] += countL / 2;
+                    if ((countL % 2) == 1) result[i + 1] += 1;
+                    countL = 0;
+                }
+            }
+            Console.WriteLine(string.Join(" ", result));
+
+        }
+    }
+}
