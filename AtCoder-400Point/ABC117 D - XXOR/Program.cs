33c82a07134f867c5b2eca24da9819069144753a
Koki Sato <agricola.basic@gmail.com>
2019-08-08 17:19:19 +0900

    ABC117-D 仕掛り

---
+++ AtCoder-400Point/ABC117 D - XXOR/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ABC117_D___XXOR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var NK = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse);
+            var Ai = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse);
+
+            var binaryK = Convert.ToString(NK[1], 2);
+            var sumXOR = new long[binaryK.Length, 2];
+            for (int digit = 0; digit < binaryK.Length; digit++)
+            {
+
+            }
+        }
+    }
+}
