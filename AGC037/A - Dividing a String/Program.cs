73bdda9f9928a27c52ee2b1b367091256b39c7b8
39raKyouko <agricola.basic@gmail.com>
2019-08-17 21:26:43 +0900

    AGC037参加　A完

---
+++ AGC037/A - Dividing a String/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace A___Dividing_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var S = Console.ReadLine();
+            var K = 1;
+            var previous = S[0].ToString();
+            var now = "";
+            for (int i = 1; i < S.Length; i++)
+            {
+                now += S[i];
+                if (now != previous)
+                {
+                    K += 1;
+                    previous = now;
+                    now = "";
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            Console.WriteLine(K);
+        }
+    }
+}
