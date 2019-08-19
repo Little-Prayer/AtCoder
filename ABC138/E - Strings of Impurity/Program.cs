fed389389f7e4d784b636e787ca04b8770860b4c
Koki Sato <agricola.basic@gmail.com>
2019-08-19 15:09:59 +0900

    ABC138E AC

--- ABC138/E - Strings of Impurity/Program.cs
+++ ABC138/E - Strings of Impurity/Program.cs
@@ -6,25 +6,57 @@ namespace E___Strings_of_Impurity
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(solver());
+        }
+
+        static long solver()
+        {
             var s = Console.ReadLine();
             var t = Console.ReadLine();
 
             var alphabets = new List<int>[26];
             for (int i = 0; i < 26; i++) alphabets[i] = new List<int>();
-            for (int i = 0; i < s.Length; i++) alphabets[s[i] - 'a'].Add(i);
-            var repeat = 0;
-            var current = 0;
+            for (int i = 0; i < s.Length; i++) alphabets[s[i] - 'a'].Add(i + 1);
+
+            long result = 0;
+            var currentIndex = 0;
             for (int i = 0; i < t.Length; i++)
             {
-                var before = current;
-                foreach (int i in alphabets[t[0] - 'a'])
+                var currentChar = alphabets[t[i] - 'a'];
+                if (currentChar.Count == 0) return -1;
+                currentIndex = binarySearch(currentChar, currentIndex);
+                if (currentIndex == -1)
                 {
-                    if (current < i)
-                    {
-                        current = i;
-                        break;
-                    }
+                    result += s.Length;
+                    currentIndex = currentChar[0];
                 }
+            }
+            return result + currentIndex;
+        }
+        static int binarySearch(List<int> list, int key)
+        {
+            int left = -1;
+            int right = list.Count;
+
+            while (right - left > 1)
+            {
+                int mid = left + (right - left) / 2;
+                if (list[mid] > key)
+                {
+                    right = mid;
+                }
+                else
+                {
+                    left = mid;
+                }
+            }
+            if (right >= list.Count)
+            {
+                return -1;
+            }
+            else
+            {
+                return list[right];
             }
         }
     }
