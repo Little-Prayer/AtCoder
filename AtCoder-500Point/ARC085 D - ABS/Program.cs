a4de92cce4c7cf68f5cdc69378dbb0bd09a3cad9
Koki Sato <agricola.basic@gmail.com>
2019-08-21 14:50:10 +0900

    ARC085 D 解説見てAC

---
+++ AtCoder-500Point/ARC085 D - ABS/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ARC085_D___ABS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var NZW = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse);
+            var Ai = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse);
+            Console.WriteLine(NZW[0] != 1 ? Math.Max(Math.Abs(Ai[Ai.Length - 1] - Ai[Ai.Length - 2]), Math.Abs(NZW[2] - Ai[Ai.Length - 1])) : Math.Abs(NZW[2] - Ai[0]));
+        }
+    }
+}
