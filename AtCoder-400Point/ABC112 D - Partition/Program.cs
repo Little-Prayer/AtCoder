29225a7be115b421eefb53c3e7605ea08b71d489
Koki Sato <agricola.basic@gmail.com>
2019-08-22 17:21:17 +0900

    ABC112 D 自力AC

---
+++ AtCoder-400Point/ABC112 D - Partition/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ABC112_D___Partition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(solver());
+        }
+        static int solver()
+        {
+            var NM = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            var N = NM[0];
+            var M = NM[1];
+            for (int i = N; i <= M / 2; i++)
+            {
+                if (M % i == 0) return M / i;
+            }
+            return 1;
+        }
+    }
+}
