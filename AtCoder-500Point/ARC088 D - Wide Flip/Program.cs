a7279b2a3eda8acaa55bbab1320118eab8107148
Koki Sato <agricola.basic@gmail.com>
2019-08-22 11:36:44 +0900

    ARC088D 解説見てAC

---
+++ AtCoder-500Point/ARC088 D - Wide Flip/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ARC088_D___Wide_Flip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(solver());
+        }
+        static int solver()
+        {
+            var S = Console.ReadLine();
+            if (S.Length == 1) return 1;
+            var result = S.Length;
+            for (int i = 1; i < S.Length; i++)
+            {
+                if (S[i] != S[i - 1]) result = Math.Min(result, Math.Max(i, S.Length - i));
+            }
+            return result;
+        }
+    }
+}
