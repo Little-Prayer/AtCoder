539a78141d8a88a720bbdd01f99f6bc1724123a8
Koki Sato <agricola.basic@gmail.com>
2019-08-23 16:09:02 +0900

    ABC118D 自力AC

---
+++ AtCoder-400Point/ABC118 D - Match Matching/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC118_D___Match_Matching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stroke = new int[] { 0, 2, 5, 5, 4, 5, 6, 3, 7, 6 };
+            var NM = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            var Ai = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse).OrderByDescending(n => n).ToArray();
+
+            var matches = new Dictionary<int, int>();
+            foreach (int i in Ai)
+            {
+                if (matches.ContainsKey(stroke[i])) continue;
+                matches.Add(stroke[i], i);
+            }
+
+            var DP = new String[NM[0] + 10];
+            for (int i = 0; i < DP.Length; i++) DP[i] = "";
+            foreach (int i in matches.Keys) DP[i] = matches[i].ToString();
+            for (int i = 0; i < NM[0]; i++)
+            {
+                if (DP[i] != "")
+                {
+                    foreach (int j in matches.Keys)
+                    {
+                        var temp = DP[i] + matches[j].ToString();
+                        if (temp.Length > DP[i + j].Length || (temp.Length == DP[i + j].Length && temp.CompareTo(DP[i + j]) > 0)) DP[i + j] = temp;
+                    }
+                }
+            }
+            Console.WriteLine(DP[NM[0]]);
+        }
+    }
+}
