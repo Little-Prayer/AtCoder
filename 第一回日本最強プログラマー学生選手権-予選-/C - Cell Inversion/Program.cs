9b1a484c6844827a59ee143b3b2397858011a168
39raKyouko <agricola.basic@gmail.com>
2019-08-25 12:56:57 +0900

    第一回日本最強プログラマー学生選手権-予選- C 自力AC

--- 第一回日本最強プログラマー学生選手権-予選-/C - Cell Inversion/Program.cs
+++ 第一回日本最強プログラマー学生選手権-予選-/C - Cell Inversion/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace C___Cell_Inversion
 {
@@ -6,12 +7,36 @@ namespace C___Cell_Inversion
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine(solver());
         }
         static long solver()
         {
             var N = int.Parse(Console.ReadLine());
             var S = Console.ReadLine();
+            long MOD = 1000000007;
+
+            if (S[0] == 'W' || S[S.Length - 1] == 'W') return 0;
+            var isRight = new bool[S.Length];
+            for (int i = 0; i < S.Length; i++) isRight[i] = (i % 2 == 1) == (S[i] == 'W');
+            if (isRight.Count(b => b) != S.Length / 2) return 0;
+
+            long setCount = 1;
+            int rightCount = 0;
+            for (int i = 0; i < S.Length; i++)
+            {
+                if (rightCount < 0) return 0;
+                if (isRight[i])
+                {
+                    rightCount++;
+                }
+                else
+                {
+                    setCount = setCount * rightCount % MOD;
+                    rightCount--;
+                }
+            }
+            for (int i = 2; i <= N; i++) setCount = setCount * i % MOD;
+            return setCount;
         }
     }
 }
