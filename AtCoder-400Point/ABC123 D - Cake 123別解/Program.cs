b50e1e7058e595fcadb31dfb4aa27c81ec697b2a
Koki Sato <agricola.basic@gmail.com>
2019-08-28 13:13:28 +0900

    ABC123 D別解作成

---
+++ AtCoder-400Point/ABC123 D - Cake 123別解/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC123_D___Cake_123別解
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var XYZK = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            var K = XYZK[3];
+            var A = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse).OrderByDescending(n => n).ToArray();
+            var B = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse).OrderByDescending(n => n).ToArray();
+            var C = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse).OrderByDescending(n => n).ToArray();
+
+            var cakes = new List<long>();
+            for (int a = 0; a < A.Length; a++)
+            {
+                for (int b = 0; b < B.Length; b++)
+                {
+                    for (int c = 0; c < C.Length; c++)
+                    {
+                        if (a * b * c > K) break;
+                        cakes.Add(A[a] + B[b] + C[c]);
+                    }
+                }
+            }
+            cakes = cakes.OrderByDescending(c => c).Take(K).ToList();
+            foreach (long c in cakes) Console.WriteLine(c);
+        }
+    }
+}
