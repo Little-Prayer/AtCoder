4a45c3429ddbf6c327e3dbd9a7f3b3dd912f6098
Koki Sato <agricola.basic@gmail.com>
2019-08-29 17:29:39 +0900

    ARC074 C 仕掛り

---
+++ AtCoder-400Point/ARC074 C - Chocolate Bar/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ARC074_C___Chocolate_Bar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var HW = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse);
+            var H = HW[0];
+            var W = HW[1];
+
+            var S = long.MaxValue;
+            for (int h = 1; h < H; h++)
+            {
+                var s1 = h * W;
+            }
+        }
+    }
+}
