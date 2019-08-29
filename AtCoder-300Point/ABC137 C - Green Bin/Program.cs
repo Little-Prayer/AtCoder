7d298961a8ef4f693f37d36f44211764e7231cb5
Koki Sato <agricola.basic@gmail.com>
2019-08-29 11:14:39 +0900

    ABC137C 自力AC

---
+++ AtCoder-300Point/ABC137 C - Green Bin/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC137_C___Green_Bin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var N = int.Parse(Console.ReadLine());
+            var strs = new string[N];
+            for (int i = 0; i < N; i++) strs[i] = Console.ReadLine();
+            int[] primes = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101 };
+            var strs2 = new long[N];
+            for (int i = 0; i < N; i++)
+            {
+                strs2[i] = 1;
+                for (int j = 0; j < strs[i].Length; j++)
+                {
+                    strs2[i] *= primes[strs[i][j] - 'a'];
+                }
+            }
+
+            strs2 = strs2.OrderBy(n => n).ToArray();
+            long result = 0;
+            long count = 1;
+            for (int i = 1; i < N; i++)
+            {
+                if (strs2[i] == strs2[i - 1])
+                {
+                    count++;
+                }
+                else
+                {
+                    result += count * (count - 1) / 2;
+                    count = 1;
+                }
+            }
+            result += count * (count - 1) / 2;
+            Console.WriteLine(result);
+        }
+
+    }
+}
--- AtCoder-400Point/ABC137 D - Summer Vacation/Program.cs
+++ AtCoder-400Point/ABC137 D - Summer Vacation/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace ABC137_D___Summer_Vacation
 {
