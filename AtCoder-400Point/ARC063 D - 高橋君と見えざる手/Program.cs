4f4d1a312d293020be8dee0f3acab83c491487ea
Koki Sato <agricola.basic@gmail.com>
2019-08-29 11:49:55 +0900

    ARC063 D 自力AC

---
+++ AtCoder-400Point/ARC063 D - 高橋君と見えざる手/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace ARC063_D___高橋君と見えざる手
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var NT = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            var N = NT[0];
+            var A = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse);
+
+            var income = new long[N];
+            var min = A[0];
+            for (int i = 1; i < N; i++)
+            {
+                income[i] = A[i] - min;
+                if (A[i] < min) min = A[i];
+            }
+
+            var maxIncome = income.Max();
+            Console.WriteLine(income.Count(i => i == maxIncome));
+        }
+    }
+}
