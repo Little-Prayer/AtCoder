9af69eda7dfaf80c2ba3a751ff9b9c6194e26e9e
Koki Sato <agricola.basic@gmail.com>
2019-08-30 17:28:11 +0900

    AGC019 B 仕掛り

---
+++ AtCoder-500Point/AGC019 B - Reverse and Compare/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AGC019_B___Reverse_and_Compare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
