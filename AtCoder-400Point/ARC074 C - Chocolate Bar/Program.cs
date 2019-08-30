b3c6b5323640fe5aff29aeb258ed21f3e7364e29
Koki Sato <agricola.basic@gmail.com>
2019-08-30 09:40:52 +0900

    ARC074 C 自力AC

--- AtCoder-400Point/ARC074 C - Chocolate Bar/Program.cs
+++ AtCoder-400Point/ARC074 C - Chocolate Bar/Program.cs
@@ -14,7 +14,22 @@ namespace ARC074_C___Chocolate_Bar
             for (int h = 1; h < H; h++)
             {
                 var s1 = h * W;
+                var s2 = Math.Max((int)((H - h) / 2) * W, (H - h) * (int)(W / 2));
+                var s3 = H * W - s1 - s2;
+                var smax = Math.Max(s1, Math.Max(s2, s3));
+                var smin = Math.Min(s1, Math.Min(s2, s3));
+                S = Math.Min(S, smax - smin);
             }
+            for (int w = 1; w < W; w++)
+            {
+                var s1 = H * w;
+                var s2 = Math.Max((int)(H / 2) * (W - w), H * (int)((W - w) / 2));
+                var s3 = H * W - s1 - s2;
+                var smax = Math.Max(s1, Math.Max(s2, s3));
+                var smin = Math.Min(s1, Math.Min(s2, s3));
+                S = Math.Min(S, smax - smin);
+            }
+            Console.WriteLine(S);
         }
     }
 }
