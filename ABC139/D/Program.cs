80a6db1f0cb2d2d7501e1f475d17f699880acb94
39raKyouko <agricola.basic@gmail.com>
2019-09-01 22:39:30 +0900

    ABC139 参加　ABCD完2WA

---
+++ ABC139/D/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace D
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var N = long.Parse(Console.ReadLine());
+            Console.WriteLine(N * (N - 1) / 2);
+        }
+    }
+}
