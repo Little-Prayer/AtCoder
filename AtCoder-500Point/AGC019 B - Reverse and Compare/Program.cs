91840eae228e08d342d3df2aebac8cbee08e1f60
Koki Sato <agricola.basic@gmail.com>
2019-09-02 09:31:38 +0900

    ABC139 B 自力AC

--- AtCoder-500Point/AGC019 B - Reverse and Compare/Program.cs
+++ AtCoder-500Point/AGC019 B - Reverse and Compare/Program.cs
@@ -6,7 +6,13 @@ namespace AGC019_B___Reverse_and_Compare
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var A = Console.ReadLine();
+            var alphabets = new long[26];
+            for (int i = 0; i < A.Length; i++) alphabets[A[i] - 'a']++;
+            long result = (long)A.Length * (long)(A.Length - 1) / 2 + 1;
+            for (int i = 0; i < 26; i++) result -= alphabets[i] * (alphabets[i] - 1) / 2;
+
+            Console.WriteLine(result);
         }
     }
 }
