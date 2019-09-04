b2df49f593542fd8b09140d09a1916ab8b5b7d55
Koki Sato <agricola.basic@gmail.com>
2019-09-04 16:13:24 +0900

    CADDi 2018 D 自力AC

---
+++ AtCoder-500Point/CADDi 2018 D - Harlequin/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CADDi_2018_D___Harlequin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var N = int.Parse(Console.ReadLine());
+            var isSecond = true;
+            for (int i = 0; i < N; i++) if (int.Parse(Console.ReadLine()) % 2 == 1) isSecond = false;
+            Console.WriteLine(isSecond ? "second" : "first");
+        }
+    }
+}
