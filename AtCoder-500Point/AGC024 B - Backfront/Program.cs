801401e232837b79855dd57cd59e14c5ff92c637
Koki Sato <agricola.basic@gmail.com>
2019-09-04 10:36:05 +0900

    AGC024 B 自力AC

---
+++ AtCoder-500Point/AGC024 B - Backfront/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AGC024_B___Backfront
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var N = int.Parse(Console.ReadLine());
+            var check = new int[N + 1];
+            var max = 0;
+            for (int i = 0; i < N; i++)
+            {
+                var read = int.Parse(Console.ReadLine());
+                check[read] = check[read - 1] + 1;
+                max = Math.Max(check[read], max);
+            }
+            Console.WriteLine(N - max);
+        }
+    }
+}
