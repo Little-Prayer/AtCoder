ee6d2bb1fed3f999da7ebe3b1913d9e724b56b25
Koki Sato <agricola.basic@gmail.com>
2019-09-05 16:46:00 +0900

    dwacon2017予選D　解説読んでAC

---
+++ AtCoder-600Point/dwacon2017予選 D-ネタだけ食べたい寿司/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace dwacon2017予選_D_ネタだけ食べたい寿司
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var NM = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            var N = NM[0];
+            var M = NM[1];
+
+            var accumSushi = new int[N + 1];
+            var netaHigh = new PriorityQueue<int>(true);
+            var benefit = new int[N + 1];
+            var netaSum = new int[N + 1];
+            for (int i = 1; i < N + 1; i++)
+            {
+                var xy = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+                var x = xy[0];
+                var y = xy[1];
+
+                accumSushi[i] = accumSushi[i - 1] + y;
+
+                var d = x - y;
+                netaHigh.Push(d);
+                netaSum[i] = netaSum[i - 1] + d;
+                if (netaHigh.Count() > M - 1)
+                {
+                    netaSum[i] -= netaHigh.Pop();
+                }
+                benefit[i] = accumSushi[i] + netaSum[i - 1] + d;
+            }
+            Console.WriteLine(benefit.Max());
+        }
+    }
+
+    class PriorityQueue<T> where T : IComparable<T>
+    {
+        private List<T> nodes;
+        private Boolean isAsc;
+
+        public PriorityQueue(Boolean _isAsc)
+        {
+            this.nodes = new List<T>();
+            this.isAsc = _isAsc;
+        }
+
+        public int Count()
+        {
+            return this.nodes.Count;
+        }
+        public T Pop()
+        {
+            T returnNode = this.nodes[0];
+            this.nodes[0] = this.nodes[nodes.Count - 1];
+            this.nodes.RemoveAt(nodes.Count - 1);
+            int root = 0;
+            while (root * 2 + 1 < this.nodes.Count)
+            {
+                int child = root * 2 + 1;
+                if (child + 1 < this.nodes.Count && compare(this.nodes[child + 1], this.nodes[child]) > 0) child += 1;
+                if (compare(this.nodes[child], this.nodes[root]) > 0)
+                {
+                    swap(child, root);
+                    root = child;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return returnNode;
+        }
+        public T Top()
+        {
+            return this.nodes[0];
+        }
+        public void Push(T t)
+        {
+            this.nodes.Add(t);
+            var addPosition = nodes.Count - 1;
+            while (addPosition > 0)
+            {
+                var parent = (int)(addPosition - 1) / 2;
+                if (compare(nodes[addPosition], nodes[parent]) > 0)
+                {
+                    swap(addPosition, parent);
+                    addPosition = parent;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        private int compare(T x, T y)
+        {
+            return this.isAsc ? y.CompareTo(x) : x.CompareTo(y);
+        }
+        private void swap(int x, int y)
+        {
+            T temp = nodes[x];
+            nodes[x] = nodes[y];
+            nodes[y] = temp;
+        }
+    }
+}
