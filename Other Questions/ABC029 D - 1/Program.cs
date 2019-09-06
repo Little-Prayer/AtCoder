96fe888e95e13feb3ae6e9953d73e29c234d7a8e
Koki Sato <agricola.basic@gmail.com>
2019-09-06 17:29:37 +0900

    ABC029 D 仕掛り

---
+++ Other Questions/ABC029 D - 1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ABC029_D___1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var N = long.Parse(Console.ReadLine());
+            var DP = new long[11, 10, 2];
+
+        }
+    }
+}
