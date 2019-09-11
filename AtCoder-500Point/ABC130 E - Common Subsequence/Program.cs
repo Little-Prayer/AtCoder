3737b2296009db3df59ac62b1d338521b696211e
Koki Sato <agricola.basic@gmail.com>
2019-09-11 17:16:15 +0900

    ABC130 E 解説読んでAC

---
+++ AtCoder-500Point/ABC130 E - Common Subsequence/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+
+namespace ABC130_E___Common_Subsequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var NM = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            var N = NM[0]; var M = NM[1];
+            var S = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            var T = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+            var DP = new ModInt[N + 1, M + 1];
+            DP[0, 0] = 1;
+            var accumDP = new ModInt[N + 2, M + 2];
+            accumDP[1, 1] = 1;
+            for (int i = 0; i < N + 1; i++)
+            {
+                for (int j = 0; j < M + 1; j++)
+                {
+                    if (i == 0 && j == 0) continue;
+                    if ((i - 1 >= 0 && j - 1 >= 0) && (S[i - 1] == T[j - 1]))
+                        DP[i, j] = accumDP[i, j];
+                    accumDP[i + 1, j + 1] = accumDP[i + 1, j] + accumDP[i, j + 1] - accumDP[i, j] + DP[i, j];
+                }
+            }
+
+            Console.WriteLine(accumDP[N + 1, M + 1]);
+
+        }
+    }
+
+    //パクリ元　https://atcoder.jp/contests/arc067/submissions/5337727
+    struct ModInt
+    {
+        const int MOD = 1000000007;
+        long Data;
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public ModInt(long data) { if ((Data = data % MOD) < 0) Data += MOD; }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static implicit operator long(ModInt modInt) => modInt.Data;
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static implicit operator ModInt(long val) => new ModInt(val);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ModInt operator +(ModInt a, ModInt b)
+        {
+            long res = a.Data + b.Data;
+            return new ModInt() { Data = res >= MOD ? res - MOD : res };
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ModInt operator +(ModInt a, long b) => a.Data + b;
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ModInt operator -(ModInt a, ModInt b)
+        {
+            long res = a.Data - b.Data;
+            return new ModInt() { Data = res < 0 ? res + MOD : res };
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ModInt operator -(ModInt a, long b) => a.Data - b;
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ModInt operator *(ModInt a, ModInt b) => new ModInt() { Data = a.Data * b.Data % MOD };
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ModInt operator /(ModInt a, ModInt b) => a.Data * GetInverse(b);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public override string ToString() => Data.ToString();
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static long GetInverse(long a)
+        {
+            long div;
+            long p = MOD;
+            long x1 = 1, y1 = 0, x2 = 0, y2 = 1;
+            while (true)
+            {
+                if (p == 1) return x2;
+                div = a / p;
+                x1 -= x2 * div;
+                y1 -= y2 * div;
+                a %= p;
+                if (a == 1) return x1;
+                div = p / a;
+                x2 -= x1 * div;
+                y2 -= y1 * div;
+                p %= a;
+            }
+        }
+    }
+}
