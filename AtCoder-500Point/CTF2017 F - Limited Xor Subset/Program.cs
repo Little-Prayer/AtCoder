a0ea72c47305992206b9601f45d2c63ee84f7a3c
Koki Sato <agricola.basic@gmail.com>
2019-09-20 17:29:02 +0900

    CTF 2017 F 仕掛り

---
+++ AtCoder-500Point/CTF2017 F - Limited Xor Subset/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CTF2017_F___Limited_Xor_Subset
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var NK = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            var N = NK[0]; var K = NK[1];
+            long MOD = 1000000007;
+            var numbers = new Dictionary<int, int>();
+            for (int i = 0; i < N; i++)
+            {
+                var a = int.Parse(Console.ReadLine());
+                if (numbers.ContainsKey(a)) numbers[a]++;
+                else numbers.Add(a, 1);
+            }
+
+            var DP = new long[numbers.Count + 1, 100000 + 1];
+            DP[0, 0] = 1;
+            var number = 0;
+            foreach (KeyValuePair<int, int> pair in numbers)
+            {
+                number++;
+                var key = pair.Key;
+                var count = pair.Value;
+                for (int i = 0; i < 100000 + 1; i++)
+                {
+                    DP[number, i] += DP[number - 1, i] * (count / 2 + 1);
+                    DP[number, i] %= MOD;
+
+                    DP[number, i % key] += DP[number - 1, i] * (count + 1) / 2;
+                    DP[number, i % key] %= MOD;
+                }
+            }
+            Console.WriteLine(DP[numbers.Count, K]);
+        }
+    }
+}
