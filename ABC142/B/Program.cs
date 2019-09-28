e5f1dac64e585d32ad9f46cd6e7079ce7bd31adb
39raKyouko <agricola.basic@gmail.com>
2019-09-28 22:18:40 +0900

    ABC142参加、ABCDE完0WA

---
+++ ABC142/B/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var NK = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            var h = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            Console.WriteLine(h.Count(i => i >= NK[1]));
+        }
+    }
+}
