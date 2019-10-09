2a4b049f49ba47f0cfef21106080e5449b73051a
Koki Sato <agricola.basic@gmail.com>
2019-10-09 16:40:12 +0900

    AGC014-B 解説読んでAC

---
+++ AtCoder-500Point/AGC014 B - Unplanned Queries/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AGC014_B___Unplanned_Queries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solve() ? "YES" : "NO");
+        }
+        static bool Solve()
+        {
+            var NM = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            var N = NM[0]; var M = NM[1];
+            var count = new int[N + 1];
+            for (int i = 0; i < M; i++)
+            {
+                var ab = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+                count[ab[0]]++;
+                count[ab[1]]++;
+            }
+            for (int i = 1; i < N + 1; i++)
+            {
+                if (count[i] % 2 == 1) return false;
+            }
+            return true;
+        }
+    }
+}
