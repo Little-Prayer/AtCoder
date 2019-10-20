b00edcb73d33769815e57402bfb7cd00791cbc44
39raKyouko <agricola.basic@gmail.com>
2019-10-20 14:25:29 +0900

    ABC143-BのO(N)回答追加

---
+++ ABC143/B別解/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace B別解
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var N = int.Parse(Console.ReadLine());
+            var d = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            var dSum = 0;
+            foreach (int i in d) dSum += i;
+
+            var result = 0;
+            foreach (int i in d)
+            {
+                dSum -= i;
+                result += dSum * i;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
