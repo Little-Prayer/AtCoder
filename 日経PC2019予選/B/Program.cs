8b4164ba5b1c3d68a762d6381976075361f48385
39raKyouko <agricola.basic@gmail.com>
2019-11-09 23:00:07 +0900

    日経PC2019参加、AB完2WA

---
+++ 日経PC2019予選/B/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(solver());
+        }
+        static long solver()
+        {
+            var N = int.Parse(Console.ReadLine());
+            var D = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            long MOD = 998244353;
+
+            if (D[0] != 0) return 0;
+
+            var distance = new long[D.Max() + 1];
+            foreach (int d in D) distance[d] += 1;
+            if (distance[0] != 1) return 0;
+            long result = 1;
+            for (int i = 1; i < distance.Length; i++)
+            {
+                for (int j = 0; j < distance[i]; j++)
+                {
+                    result *= distance[i - 1];
+                    result %= MOD;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ 日経PC2019予選/C/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(solver() ? "Yes" : "No");
+        }
+        static bool solver()
+        {
+            var N = int.Parse(Console.ReadLine());
+            var A = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse);
+            var B = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse);
+
+            var sortA = A.OrderBy(n => n).ToArray();
+            var sortB = B.OrderBy(n => n).ToArray();
+            for (int i = 0; i < N; i++) if (sortA[i] > sortB[i]) return false;
+            for (int i = 1; i < N; i++) if (sortA[i] <= sortB[i - 1]) return true;
+
+            var dicA = new Dictionary<long, int>();
+            var dicB = new Dictionary<long, int>();
+            for (int i = 0; i < N; i++)
+            {
+                dicA.Add(sortA[i], i);
+                dicB.Add(sortB[i], i);
+            }
+
+            var orderA = new int[N];
+            var orderB = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                orderA[i] = dicA[A[i]];
+                orderB[i] = dicB[B[i]];
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                if (orderA[i] == orderB[i]) return true;
+                if (orderB[i] == orderA[orderA[i]]) return true;
+            }
+            return false;
+        }
+    }
+}
