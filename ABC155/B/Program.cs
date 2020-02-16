e3e6eb3e38dada49e33117a5ee6a84cad8ffbcfc
39raKyouko <agricola.basic@gmail.com>
2020-02-16 22:40:40 +0900

    ABC155参加、ABE完

---
+++ ABC155/B/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(solver() ? "APPROVED" : "DENIED");
+        }
+        static bool solver()
+        {
+            var N = int.Parse(Console.ReadLine());
+            var A = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            foreach (int a in A)
+            {
+                if ((a % 2) == 0)
+                {
+                    if (((a % 3) != 0) && ((a % 5) != 0)) return false;
+                }
+
+            }
+            return true;
+        }
+    }
+}
