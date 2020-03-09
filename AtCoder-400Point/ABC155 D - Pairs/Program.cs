aac67c5127ea14f92886de9299f926ca6fbc8090
Koki Sato <agricola.basic@gmail.com>
2020-03-09 17:02:04 +0900

    ABC155-D仕掛

--- AtCoder-400Point/ABC155 D - Pairs/Program.cs
+++ AtCoder-400Point/ABC155 D - Pairs/Program.cs
@@ -7,24 +7,35 @@ namespace ABC155_D___Pairs
     {
         static long[] A;
         static long[] reverseA;
-        static int N;
+        static long N;
         static long K;
         static void Main(string[] args)
         {
-            var NK = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            var NK = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse);
             N = NK[0]; K = NK[1];
             A = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse).OrderBy(n => n).ToArray();
             reverseA = A.Reverse().ToArray();
+
+            var result = lower_bound(-1000000000000000001, 1000000000000000001, a => CountLessThanX(a) >= K);
+            Console.WriteLine(result);
         }
 
-        static int CountLessThanX(long X)
+        static long CountLessThanX(long X)
         {
-            var result = 0;
+            long result = 0;
             foreach (long n in A)
             {
-                var devide = X / n;
-                var subtotal = lower_bound(-1, N, a => A[a] <= devide);
+                if (n > 0)
+                {
+                    result += lower_bound(-1, N, a => n * A[a] > X);
+                }
+                else
+                {
+                    result += lower_bound(-1, N, a => n * reverseA[a] > X);
+                }
+                if (n * n <= X) result -= 1;
             }
+            return result / 2;
         }
         static long lower_bound(long ng, long ok, Func<long, bool> pred)
         {
