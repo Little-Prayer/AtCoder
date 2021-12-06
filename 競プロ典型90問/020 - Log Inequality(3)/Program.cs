5500b0d5f5a210abe6a46746946d76c0520a6ec5
Koki Sato <agricola.basic@gmail.com>
2021-12-06 16:33:15 +0900

    典型90-20AC

---
+++ 競プロ典型90問/020 - Log Inequality(3)/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _020___Log_Inequality_3_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var abc = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse);
+            var temporary = abc[2];
+            for (int i = 1; i < abc[1]; i++) temporary *= abc[2];
+
+            Console.WriteLine(abc[0] < temporary ? "Yes" : "No");
+        }
+    }
+}
