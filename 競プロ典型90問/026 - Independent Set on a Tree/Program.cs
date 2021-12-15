b2f2d8bb4b2cab4cbd66e615706b401888daa273
Koki Sato <agricola.basic@gmail.com>
2021-12-15 13:36:06 +0900

    典型90-26AC

---
+++ 競プロ典型90問/026 - Independent Set on a Tree/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace _026___Independent_Set_on_a_Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var N = int.Parse(Console.ReadLine());
+            var tree = new List<int>[N + 1];
+            for (int i = 0; i < N + 1; i++) tree[i] = new List<int>();
+
+            for (int i = 0; i < N - 1; i++)
+            {
+                var ab = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+                tree[ab[0]].Add(ab[1]);
+                tree[ab[1]].Add(ab[0]);
+            }
+
+            var seen = new bool[N + 1];
+            var divide = new bool[N + 1];
+            dfs(1, true);
+
+            void dfs(int current, bool div)
+            {
+                if (seen[current]) return;
+                seen[current] = true;
+                divide[current] = div;
+                foreach (int next in tree[current]) dfs(next, !div);
+            }
+            int[] result;
+            if (divide.Count(b => b) >= N / 2)
+                result = divide.Select((b, i) => (b, i)).Where(x => x.b).Take(N / 2).Select(x => x.i).ToArray();
+            else result = divide.Select((b, i) => (b, i)).Where(x => !x.b).Skip(1).Take(N / 2).Select(x => x.i).ToArray();
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
