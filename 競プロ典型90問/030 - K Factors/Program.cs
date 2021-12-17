34544b31dc9b200690a407d09d8b85d0c49bedd9
Koki Sato <agricola.basic@gmail.com>
2021-12-17 17:14:05 +0900

    典型90-30解説AC

---
+++ 競プロ典型90問/030 - K Factors/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _030___K_Factors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var NK = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            var divisor = new int[NK[0] + 1];
+
+            for (int i = 2; i < NK[0] + 1; i++)
+            {
+                if (divisor[i] > 0) continue;
+                for (int j = i; j <= NK[0]; j += i) divisor[j]++;
+            }
+            Console.WriteLine(divisor.Count(n => n >= NK[1]));
+        }
+    }
+}
