385dfa3628fc5e99f67c016cdd5fa3cb68e2a2d1
39raKyouko <agricola.basic@gmail.com>
2022-01-15 22:40:16 +0900

    ABC235 ABC完

---
+++ ABC235/B/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var N = int.Parse(Console.ReadLine());
+            var H = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+            var result = H[0];
+            for (int i = 1; i < N; i++)
+            {
+                if (H[i] > H[i - 1])
+                    result = H[i];
+                else break;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ ABC235/C/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+namespace C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var NQ = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            var N = NQ[0]; var Q = NQ[1];
+            var a = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+            var dic = new Dictionary<int, List<int>>();
+            for (int i = 0; i < N; i++)
+            {
+                if (!dic.ContainsKey(a[i]))
+                {
+                    var item = new List<int>();
+                    item.Add(i + 1);
+                    dic.Add(a[i], item);
+                }
+                else
+                {
+                    dic[a[i]].Add(i + 1);
+                }
+            }
+
+            for (int i = 0; i < Q; i++)
+            {
+                var q = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+                var qa = q[0]; var qb = q[1];
+                if (dic.ContainsKey(qa))
+                {
+                    if (dic[qa].Count < qb) Console.WriteLine(-1);
+                    else Console.WriteLine(dic[qa][qb - 1]);
+                }
+                else Console.WriteLine(-1);
+            }
+        }
+    }
+}
