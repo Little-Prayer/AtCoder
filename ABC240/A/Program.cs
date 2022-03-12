64dddf012ff293b7a4fd99bd5b78f7b39d50346d
39raKyouko <agricola.basic@gmail.com>
2022-03-13 00:40:16 +0900

    ABC240参加

---
+++ ABC240/A/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ab = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            var a = ab[0]; var b = ab[1];
+            Console.WriteLine(Math.Abs(a - b) == 1 || Math.Abs(a - b) == 9 ? "Yes" : "No");
+        }
+    }
+}
