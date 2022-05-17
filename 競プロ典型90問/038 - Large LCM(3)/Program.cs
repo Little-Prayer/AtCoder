6119447f2430a5c92f31988d0ee418a4bb62bf1a
Koki Sato <agricola.basic@gmail.com>
2022-05-17 16:35:09 +0900

    典型90-38,39AC

---
+++ 競プロ典型90問/038 - Large LCM(3)/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _038___Large_LCM_3_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse);
+            var A = input[0]; var B = input[1];
+
+
+            var gcd = GCD(A, B);
+            if (1000000000000000000 / B < A / gcd) Console.WriteLine("Large");
+            else Console.WriteLine(A / gcd * B);
+
+        }
+
+        static long GCD(long a, long b)
+        {
+            if (a < b) return GCD(b, a);
+
+            if (a % b == 0) return b;
+
+            return GCD(b, a % b);
+        }
+    }
+}
---
+++ 競プロ典型90問/039 - Tree Distance(5)/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _039___Tree_Distance_5_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var N = long.Parse(Console.ReadLine());
+            var tree = new node[N + 1];
+            for (int i = 0; i <= N; i++) tree[i] = new node();
+            for (int i = 0; i < N - 1; i++)
+            {
+                var ab = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+                tree[ab[0]].connections.Add(ab[1]);
+                tree[ab[1]].connections.Add(ab[0]);
+            }
+
+            long countChildren(int parent, int current)
+            {
+                tree[current].parent = parent;
+                var children = tree[current].connections.Where(x => x != parent);
+                if (children.Count() == 0) tree[current].descendant = 1;
+                else tree[current].descendant = children.Sum(x => countChildren(current, x)) + 1;
+
+                return tree[current].descendant;
+            }
+            int root = tree.Select((node, i) => (node, i)).Where(x => x.node.connections.Count == 1).First().i;
+            countChildren(0, root);
+
+            Console.WriteLine(tree.Sum(t => t.descendant * (N - t.descendant)));
+        }
+    }
+
+    class node
+    {
+        public int parent;
+        public List<int> connections;
+
+        public long descendant;
+        public node() { connections = new List<int>(); }
+
+    }
+}
