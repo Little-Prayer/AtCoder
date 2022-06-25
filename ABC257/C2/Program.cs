8e0a4995b3124dc7179bd75becfad0a0c6b3ddb8
39raKyouko <agricola.basic@gmail.com>
2022-06-25 23:52:12 +0900

    ABC257-C再回答

--- ABC257/C2/Program.cs
+++ ABC257/C2/Program.cs
@@ -8,9 +8,21 @@ namespace C2
         static void Main(string[] args)
         {
             var N = int.Parse(Console.ReadLine());
-            var isAdult = Console.ReadLine().Select(c => c == '1');
+            var isAdult = Console.ReadLine().Select(c => c == '1').ToArray();
             var W = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
             var sortedW = W.Select((w, index) => (w, index)).OrderBy(c => c.w).ToArray();
+
+            var current = isAdult.Where(b => b).Count();
+            var max = current;
+
+            for (int i = 0; i < N; i++)
+            {
+                current += isAdult[sortedW[i].index] ? -1 : 1;
+                if (i < N - 1 && sortedW[i].w != sortedW[i + 1].w)
+                    max = Math.Max(max, current);
+            }
+            max = Math.Max(max, current);
+            Console.WriteLine(max);
         }
     }
 }
