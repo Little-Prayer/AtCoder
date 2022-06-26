1796016d9ab0090dd4cab84314c27ff99c136035
39raKyouko <agricola.basic@gmail.com>
2022-06-26 23:05:46 +0900

    ARC143 1AC

---
+++ ARC143/A/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(solver());
+        }
+
+        static long solver()
+        {
+            var ABC = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse);
+            var A = ABC[0]; var B = ABC[1]; var C = ABC[2];
+            var max = Math.Max(Math.Max(A, B), C);
+            var sum = A + B + C;
+            if (sum - max < max) return -1;
+            else return max;
+        }
+    }
+}
---
+++ ARC143/B/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var N = long.Parse(Console.ReadLine());
+            long MOD = 998244353;
+
+            var permutation = new long[N * N + 1];
+            for (long i = 1; i < N * N + 1; i++)
+            {
+                long result = 1;
+                for (long j = 0; j < N - 1; j++)
+                {
+                    result *= i - j;
+                    result %= MOD;
+                }
+                permutation[i] = result;
+            }
+
+            long all = 1;
+            for (long i = 1; i <= N * N; i++)
+            {
+                all *= i;
+                all %= MOD;
+            }
+
+            long temp = 1;
+            for (int i = 1; i <= N * N - 2 * N + 1; i++)
+            {
+                temp *= i;
+                temp %= MOD;
+            }
+
+            long allresult = 0;
+            for (long i = 1; i <= N * N; i++)
+            {
+                long current = 1;
+                current *= permutation[i - 1];
+                current %= MOD;
+                current *= permutation[N * N - i];
+                current %= MOD;
+                current *= N * N;
+                current %= MOD;
+                current *= temp;
+                current %= MOD;
+                allresult += current;
+                allresult %= MOD;
+            }
+            allresult = all - allresult;
+            if (allresult < 0) allresult += MOD;
+            Console.WriteLine(N == 1 ? 0 : allresult);
+        }
+    }
+}
