5f37102f9bd416ed2a5ffcc86ceb525c0880fa9e
39raKyouko <agricola.basic@gmail.com>
2022-07-30 23:00:26 +0900

    ARC145参加　2完1WA

---
+++ ARC145/A/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var N = int.Parse(Console.ReadLine());
+            var S = Console.ReadLine();
+            Console.WriteLine(solver(S) ? "Yes" : "No");
+        }
+
+        static bool solver(string S)
+        {
+            var arrayS = S.ToCharArray();
+            if (S.Length == 2) return S[0] == S[1];
+
+            if (S[0] == 'A' && S[S.Length - 1] == 'B') return false;
+
+            return true;
+        }
+    }
+}
---
+++ ARC145/B/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(solver());
+        }
+        static long solver()
+        {
+            var NAB = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse);
+            var N = NAB[0]; var A = NAB[1]; var B = NAB[2];
+
+            return N >= A ? ((N / A) - 1) * Math.Min(A, B) + Math.Min((N % A) + 1, B) : 0;
+        }
+    }
+}
