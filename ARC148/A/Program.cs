f42b565d1bf62d60a4e7578e0dd767a49597ed8a
39raKyouko <agricola.basic@gmail.com>
2022-09-11 23:01:59 +0900

    ARC148参加

---
+++ ARC148/A/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(solver());
+        }
+        static int solver()
+        {
+            var N = int.Parse(Console.ReadLine());
+            var A = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+            var Amin = A.Min();
+            A = A.Select(n => n - Amin).ToArray();
+
+            var divisor = new List<int>();
+            var Amax = A.Max();
+            if (Amax == 0) return 1;
+            if (Amax == 1) return 2;
+
+            for (int i = 2; i < Math.Sqrt(Amax); i++)
+            {
+                if ((Amax % i) == 0)
+                {
+                    divisor.Add(i);
+                    divisor.Add(Amax / i);
+                }
+            }
+            if (divisor.Count() == 0) return 2;
+
+            var result = true;
+            foreach (int i in divisor)
+            {
+                result = true;
+                foreach (int j in A)
+                {
+                    if ((j % i) > 0)
+                    {
+                        result = false;
+                        break;
+                    }
+
+                }
+                if (result)
+                    break;
+            }
+            return result ? 1 : 2;
+        }
+    }
+}
---
+++ ARC148/C/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var NQ = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            var N = NQ[0]; var Q = NQ[1];
+            var parent = new int[N + 1];
+            var child = new int[N + 1];
+
+            var read = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            for (int j = 0; j < N - 1; j++)
+            {
+                parent[j + 2] = read[j];
+                child[read[j]]++;
+            }
+
+
+            var coin = new bool[N + 1];
+            for (int i = 0; i < Q; i++)
+            {
+                for (int j = 0; j < N + 1; j++) coin[j] = false;
+                var read2 = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+                for (int j = 1; j <= read2[0]; j++) coin[read2[j]] = true;
+
+                var count = 0;
+                foreach (int r in read2.Skip(1))
+                {
+                    count += child[r];
+                    if (coin[parent[r]]) count--;
+                    else count++;
+                }
+                Console.WriteLine(count);
+            }
+        }
+    }
+}
