76e50e45e79c96efead58284ada0aa8c03ee9c3f
39raKyouko <agricola.basic@gmail.com>
2023-01-14 20:58:58 +0900

    ABC283参加

---
+++ ABC283/A/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var AB = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse);
+            var A = AB[0]; var B = AB[1];
+            long result = 1;
+
+            for (int i = 0; i < B; i++)
+            {
+                result *= A;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
